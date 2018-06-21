60c516838f58e5f98d1ccf936850c4ff1aae418f
bingjie.huang <bingjie.huang@shuyun.com>
2018-06-21 21:52:34 +0800

    注册日志系统

--- src/game.ts
+++ src/game.ts
@@ -4,6 +4,7 @@ import { InputSystem } from "./systems/input/input"
 import { ChangeStage } from "@components/Stage/messages/ChangeStage";
 import { WelcomeSystem } from "@systems/welcome/Welcome";
 import { Welcome } from "@components/Welcome/messages/Welcome";
+import { LogSystem } from "@systems/log/log";
 
 export class Game {
   public entities = new Map()
@@ -14,6 +15,7 @@ export class Game {
     this.system.actorOf(new StageSystem, "StageSystem")
     this.system.actorOf(new InputSystem, "InputSystem")
     this.system.actorOf(new WelcomeSystem, "WelcomSystem")
+    this.system.actorOf(new LogSystem, "LogSystem")
 
 
     this.system.tell("WelcomeSystem", new Welcome())
